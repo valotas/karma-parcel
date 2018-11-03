740e94e2f9506db525a4484faec6a06163cf8545
Georgios Valotasios <valotas@gmail.com>
2018-11-03 10:03:49 +0100

    file utility classes

---
+++ src/files.Spec.ts
@@ -0,0 +1,63 @@
+// eslint-env mocha
+import * as assert from "assert";
+import * as fs from "fs";
+import * as os from "os";
+import * as sinon from "sinon";
+import { promisify } from "util";
+import { BundleFile, EntryFile } from "./files";
+
+describe("files", () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it("exports EntryFile", () => {
+    assert.ok(EntryFile);
+  });
+
+  it("exports BundleFile", () => {
+    assert.ok(BundleFile);
+  });
+
+  [EntryFile, BundleFile].forEach(Const => {
+    describe(Const.name, () => {
+      it("is created without any args", () => {
+        const file = new Const();
+        assert.ok(file);
+      });
+
+      it("has a dir equal to os.tmpdir", () => {
+        const file = new Const();
+        assert.equal(file.dir, os.tmpdir());
+      });
+
+      it("has an name based on type and current date", () => {
+        sinon.stub(Date, "now").returns("111");
+        const file = new Const();
+        assert.equal(
+          file.name,
+          `karma-parcel-${Const === EntryFile ? "entry" : "bundle"}-111.js`
+        );
+      });
+
+      if (Const === EntryFile) {
+        it("can be touched", done => {
+          const file = new EntryFile();
+          file.touch().then(() => {
+            fs.stat(file.path, done);
+          });
+        });
+
+        it("allows addition of files", () => {
+          const file = new EntryFile();
+          return file
+            .add("/path/to/file")
+            .then(() => promisify(fs.readFile)(file.path))
+            .then(cont => {
+              assert.equal(cont, `import "/path/to/file";`);
+            });
+        });
+      }
+    });
+  });
+});
---
+++ src/files.ts
@@ -0,0 +1,57 @@
+import * as fs from "fs";
+import * as os from "os";
+import * as path from "path";
+import { promisify } from "util";
+
+class TmpFile {
+  dir: string;
+  name: string;
+
+  path: string;
+
+  constructor(suffix: string) {
+    this.dir = os.tmpdir();
+    this.name = `karma-parcel-${suffix}-${Date.now()}.js`;
+    this.path = path.join(this.dir, this.name);
+  }
+
+  exists() {
+    return promisify(fs.exists)(this.path);
+  }
+}
+
+export class BundleFile extends TmpFile {
+  constructor() {
+    super("bundle");
+  }
+}
+
+export class EntryFile extends TmpFile {
+  private files: string[];
+  private done = Promise.resolve();
+
+  constructor() {
+    super("entry");
+    this.files = [];
+  }
+
+  add(path: string) {
+    this.files.push(path);
+    const content = this.files.map(f => `import "${f}";`).join("\n");
+    return this.write(content);
+  }
+
+  async touch() {
+    const exists = await this.exists();
+    if (!exists) {
+      return this.write("");
+    }
+  }
+
+  write(content: string) {
+    this.done = this.done.then(() =>
+      promisify(fs.writeFile)(this.path, content)
+    );
+    return this.done;
+  }
+}
